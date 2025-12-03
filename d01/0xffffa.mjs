416def744e38f9dc933ba67ccc0f95642f26702c
Hyeonsong Kim <kimhxsong@gmail.com>
2025-12-03 12:02:54 +0900

    optimize day 2

--- d01/0xffffa.mjs
+++ d01/0xffffa.mjs
@@ -7,14 +7,37 @@ let sumOfInvalid = 0;
 
 const checkInvalid = (num) => {
   const nstr = num.toString();
-  for (let i = 0; i < nstr.length / 2; i++) {
-    const j = nstr.length / 2 + i;
-    if (nstr[i] !== nstr[j]) {
+  const len = nstr.length;
+
+  if (len % 2 !== 0) return false; // 홀수 길이 제외
+
+  const half = len >> 1;
+  for (let i = 0; i < half; i++) {
+    if (nstr.charCodeAt(i) !== nstr.charCodeAt(half + i)) {
       return false;
     }
   }
   return true;
 };
+// ________________________________________________________
+// Executed in  110.15 millis    fish           external
+//    usr time  103.15 millis  154.00 micros  103.00 millis
+//    sys time   17.48 millis  698.00 micros   16.78 millis
+
+// const checkInvalid = (num) => {
+//   const nstr = num.toString();
+//   for (let i = 0; i < nstr.length / 2; i++) {
+//     const j = nstr.length / 2 + i;
+//     if (nstr[i] !== nstr[j]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+// ________________________________________________________
+// Executed in  172.57 millis    fish           external
+//    usr time  175.47 millis  180.00 micros  175.28 millis
+//    sys time   18.20 millis  827.00 micros   17.38 millis
 
 for (const range of ranges) {
   const start = +range[0];
--- d01/0xffffb.mjs
+++ d01/0xffffb.mjs
@@ -5,12 +5,10 @@ const ranges = input.split(",").map((range) => range.split("-"));
 let count = 0;
 let sumOfInvalid = 0;
 
-const isRepeatingPattern = (nstr, patternLength) => {
-  const pattern = nstr.slice(0, patternLength);
-
-  for (let i = 0; i < nstr.length; i += patternLength) {
-    const expected = nstr.slice(i, i + patternLength);
-    if (expected !== pattern) {
+// After optimization
+const isRepeatingPattern = (nstr, pl) => {
+  for (let i = pl; i < nstr.length; i++) {
+    if (nstr.charCodeAt(i) !== nstr.charCodeAt(i % pl)) {
       return false;
     }
   }
@@ -19,8 +17,10 @@ const isRepeatingPattern = (nstr, patternLength) => {
 
 const checkInvalid = (num) => {
   const nstr = num.toString();
-  for (let pl = 1; pl <= Math.floor(nstr.length / 2); pl++) {
-    if (isRepeatingPattern(nstr, pl)) {
+  const len = nstr.length;
+
+  for (let pl = 1; pl <= len >> 1; pl++) {
+    if (len % pl === 0 && isRepeatingPattern(nstr, pl)) {
       return true;
     }
   }
@@ -32,11 +32,57 @@ for (const range of ranges) {
   const end = +range[1];
   for (let current = start; current <= end; current++) {
     if (checkInvalid(current)) {
-      count++;
       sumOfInvalid += current;
-      // console.log(current);
     }
   }
 }
+// ________________________________________________________
+// Executed in  123.75 millis    fish           external
+//    usr time  120.63 millis  169.00 micros  120.46 millis
+//    sys time   16.12 millis  744.00 micros   15.38 millis
 
 console.log(sumOfInvalid);
+
+// Before optimization
+// const isRepeatingPattern = (nstr, patternLength) => {
+//   const pattern = nstr.slice(0, patternLength);
+
+//   for (let i = 0; i < nstr.length; i += patternLength) {
+//     const expected = nstr.slice(i, i + patternLength);
+//     if (expected !== pattern) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
+// const checkInvalid = (num) => {
+//   const nstr = num.toString();
+//   for (let pl = 1; pl <= Math.floor(nstr.length / 2); pl++) {
+//     if (isRepeatingPattern(nstr, pl)) {
+//       return true;
+//     }
+//   }
+//   return false;
+// };
+
+// for (const range of ranges) {
+//   const start = +range[0];
+//   const end = +range[1];
+//   for (let current = start; current <= end; current++) {
+//     if (checkInvalid(current)) {
+//       count++;
+//       sumOfInvalid += current;
+//       // console.log(current);
+//     }
+//   }
+// }
+
+// ╰─❯ time node 0xffffb.mjs input.txt                              (base)
+// 15704845910
+
+// ________________________________________________________
+// Executed in  307.76 millis    fish           external
+//    usr time  288.28 millis  170.00 micros  288.11 millis
+//    sys time   24.36 millis  665.00 micros   23.69 millis
+// --------------------------------------------------------
